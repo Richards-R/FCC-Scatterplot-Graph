6362605428ea516ac69ff94105739c8ef4ef9e5f
Reon-Richards <rr7366@gmail.com>
2023-06-29 22:05:17 +0900

    completed project

--- scatterplot.js
+++ scatterplot.js
@@ -19,17 +19,21 @@ const w = 600;
 const h = 500;
 const padding = 50;
 
+var div = d3
+  .select('body')
+  .append('div')
+  .attr('class', 'tooltip')
+  .attr('id', 'tooltip')
+  .style('opacity', 0);
+
 const svg = d3.select('svg')
-    .attr("width", w)
-    .attr("height", h)
-
-
+  .attr("width", w)
+  .attr("height", h)
 
 req.open("GET", url, true);
 req.onload = () => {
   json = JSON.parse(req.responseText)
-                                  console.log(json.length)
-                                  console.log(json[0].Year)
+
   for (let i=0; i<json.length; i++){
   scattArr.push([json[i].Year, json[i].Time, !json[i].Doping, json[i].Name, json[i].Nationality, json[i].Doping])}
 
@@ -37,12 +41,9 @@ req.onload = () => {
   generateAxes()
   drawDots()
   generateLegend()
-   
   };
 
 req.send();
-
-console.log(tooltipArr)
 
 const generateScales = () => {
   yearArr = scattArr.map((item) => {
@@ -67,16 +68,15 @@ const generateScales = () => {
               
   for (let i=0; i<json.length; i++){
     dotArr.push([scattArr[i][0], totalSecondsArr[i], scattArr[i][2], scattArr[i][1], scattArr[i][3], scattArr[i][4], scattArr[i][5]])}  
-    console.log(dotArr)
 
   xAxisScale = d3.scaleLinear()
-    .domain([d3.min(yearArr), d3.max(yearArr)])
+    .domain([d3.min(yearArr)-1, d3.max(yearArr)+1])
     .range([padding, w - padding])
   
   yAxisScale = d3.scaleLinear()
     .domain([d3.max(totalSecondsArr), d3.min(totalSecondsArr)])
     .range([h-padding, padding])
-    }
+}
 
 const  generateAxes = () =>{
   let xAxis = d3.axisBottom(xAxisScale)
@@ -93,8 +93,8 @@ const  generateAxes = () =>{
     .call(yAxis)
     .attr("transform", "translate (" + (padding) + ", 0)")
     .attr('id','y-axis')
-    }
-  
+}
+
 const drawDots = () => {
   svg.selectAll("dots")
     .data(dotArr)
@@ -105,41 +105,41 @@ const drawDots = () => {
     .attr("data-xvalue", (d)=>(d[0]))
     .attr("cy",  (d)=>((yAxisScale(d[1]))))
     .attr("data-yvalue",  (d)=>(new Date(d[1] * 1000)))
-    .attr("stroke", "rgb(46, 95, 114)")
-    .attr("opacity", "0.5")
-    .attr("fill", (d)=> d[2] === false ? "green" : "orange")
-    .on("mouseover", (event, item)=>{
-      drawToolTip.transition()
-      .style("visibility", "visible")
-      drawToolTip.text(`${item[4]} (${item[5]}) ${item[0]}　Time ${item[3]}　${item[6]}`)
-      document.querySelector("#tooltip").setAttribute("data-year", item[0])
-    })
-    .on("mouseout", (event, item) => {
-      drawToolTip.transition()
-      .style("visibility", "hidden")
-    })
-}
-    
+    .attr("fill", (d)=> d[2] === false ? "url(#dopeGrad)" : "url(#cleanGrad)")
 
-let generateLegend = () => svg.append("rect")
+    .on('mouseover', function (event, item) {
+      div.style('opacity', 0.9);
+      div.attr('data-year',  item[0]);
+      div
+        .html(
+          item[4] +
+            ': ' +
+            item[5]  +
+            '<br/>' +
+            'Year: ' +
+            item[0] +
+            ', Time: ' +
+            item[3] +
+            (item[6] ? '<br/>' + item[6] : '')
+        )
+        .style('left', event.pageX + 'px')
+        .style('top', event.pageY - 28 + 'px');
+    })
+    .on('mouseout', function () {
+      div.style('opacity', 0);
+    });
+}
+
+let generateLegend = () => {
+  svg.append("rect")
     .attr("width", "205px")
     .attr("height", "54px")
     .attr("x", "380px")
     .attr("y", "180px")
     .attr("fill", "none")
-    .attr("stroke", "rgb(46, 95, 114)")
+    .attr("stroke", "silver")
     .attr("id", "legend")
-
-      console.log(tooltipArr)
-  
-  let drawToolTip = d3.select("body")
-      .append("div")
-      .attr("id", "tooltip" )
-      .attr("x","40" )
-      .attr("y", "50%")
-      .style("width", "auto")
-      .style("height", "auto")
-      .style("visibility", "hidden")
+}
 
 
 
